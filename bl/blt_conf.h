0da9eae87334be6b9743c023ce21f0e2717fd279
hvs <hvs@pwning>
2024-01-29 21:31:33 +0800

    bl: enable openblt nvm hook

--- bl/blt_conf.h
+++ bl/blt_conf.h
@@ -131,7 +131,7 @@
  * BOOT_NVM_CHECKSUM_HOOKS_ENABLE.
  */
 /** \brief Enable/disable the NVM hook function for supporting additional memory devices. */
-#define BOOT_NVM_HOOKS_ENABLE           (0)
+#define BOOT_NVM_HOOKS_ENABLE           (1)
 /** \brief Configure the size of the default memory device (typically flash EEPROM). */
 #define BOOT_NVM_SIZE_KB                (512)
 /** \brief Enable/disable hooks functions to override the user program checksum handling. */
